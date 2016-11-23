cd5d788a15fcca7d12f74cf2644f6dd24f028799
Daniel <dfzunigah@unal.edu.co>
2016-11-23 11:08:56 -0500

    updated, showing feedback

--- ReviewListener.java
+++ ReviewListener.java
@@ -1,34 +1,100 @@
 package ops;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ReviewListener extends MouseAdapter {
 
     private JButton button;
+    private JButton statusIcon;
     private FileWriter writer;
+    private Scanner reader;
+    private ArrayList<Integer> puntuations;
 
-    public ReviewListener(JButton one) {
+    public ReviewListener(JButton one, JButton statusIcon) {
         this.button = one;
+        this.statusIcon = statusIcon;
+        reader = new Scanner("puntuation.txt");
+        puntuations = new ArrayList<Integer>();
     }
 
+    @Override
     public void mousePressed(MouseEvent e) {
         button.setBorder(BorderFactory.createLoweredBevelBorder());
         try {
             writer = new FileWriter("puntuation.txt", true);
             writer.write(button.getText() + " ");
             writer.close();
-        } catch (IOException ex) {}
+        } catch (IOException ex) {
+            ex.getStackTrace();
+        }
     }
 
+    @Override
     public void mouseReleased(MouseEvent e) {
         button.setBorder(null);
+        this.getPuntuation();
+        this.updateImagePuntuation();
+    }
+    
+    public void getPuntuation(){
+        try{
+            puntuations.clear();
+            File file = new File("puntuation.txt");
+            reader = new Scanner(file);
+            while(reader.hasNext()){
+                puntuations.add(reader.nextInt());
+            }
+            reader.close();
+        }catch(FileNotFoundException e){
+             e.getStackTrace();
+        }
+    }
+    
+    public double status(){
+        if(puntuations.isEmpty()){
+            return 0;
+        }
+        int total=0;
+        for(int base : puntuations){
+            total+=base;
+        }
+        double average=((double)total/puntuations.size());
+        return round(average);
+    }
+    
+    public static double round(double value) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+    
+    public void updateImagePuntuation(){
+        double value = this.status();
+        if(value>0.0 && value<=1.0){
+            statusIcon.setIcon(new ImageIcon(getClass().getResource("/Resources/Rated1.png")));
+        }else if(value>1.0 && value<=2.0){
+            statusIcon.setIcon(new ImageIcon(getClass().getResource("/Resources/Rated2.png")));
+        }else if(value>2.0 && value<=3.0){
+            statusIcon.setIcon(new ImageIcon(getClass().getResource("/Resources/Rated3.png")));
+        }else if(value>3.0 && value<=4.0){
+            statusIcon.setIcon(new ImageIcon(getClass().getResource("/Resources/Rated4.png")));
+        }else if(value>4.0 && value<=5.0){
+            statusIcon.setIcon(new ImageIcon(getClass().getResource("/Resources/Rated5.png")));
+        }else{
+            statusIcon.setIcon(new ImageIcon(getClass().getResource("/Resources/Rated5.png")));
+        }
     }
 
 }
